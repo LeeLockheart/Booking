e5f8de2c4ca85ca0bc69eff3477c092d0bb8709e
LeeLockheart <86777250+LeeLockheart@users.noreply.github.com>
2021-11-03 19:32:28 +0530

    Create admin.py

---
+++ advisorapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from advisorapp.models import addadvisor
+from advisorapp.models import advdisplay
+from advisorapp.models import db
+from advisorapp.models import users
+from advisorapp.models import Bookadvisor1
+from advisorapp.models import info
+
+admin.site.register(addadvisor)
+admin.site.register(advdisplay)
+admin.site.register(db)
+admin.site.register(users)
+admin.site.register(Bookadvisor1)
+admin.site.register(info)
